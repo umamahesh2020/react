7245cd93abeb114208f1f0071ef1fa069c4bb3f0
umamahesh2020 <umamahesh2020@gmail.com>
2020-05-24 22:24:41 +0530

    Updates

---
+++ precache-manifest.4d977472dcfa511d3417083eb1c06396.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cd97684542d6bfcc8da07749cf7fcc",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "55c74c077808b6d87b68",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/react/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c74c077808b6d87b68",
+    "url": "/react/static/js/main.05dd999a.chunk.js"
+  },
+  {
+    "revision": "10c2fc0778cfd83222f9",
+    "url": "/react/static/js/runtime-main.b7f0fd33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
